c550e6c132c38e462512448ade44cd40211b5cc0
BeauRussell <yourknightmares@gmail.com>
2024-11-27 19:47:48 -0600

    Set up basic WS Server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-import WebSocket from 'ws';
+import {WebSocket, WebSocketServer} from 'ws';
+
+const server: WebSocketServer = new WebSocketServer({ port: 8080 });
+
+server.on('connection', (ws: WebSocket) => {
+
+    ws.on('message', (message: string) => {
+        console.log(message);
+        ws.send('Server Received your message.');
+    });
+
+    ws.on('close', () => {
+       console.log('Client Disconnected');
+       ws.close();
+    });
+});
